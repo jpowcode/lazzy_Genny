e0068233c505935475c75fc62100d22bf32bca22
jpowcode <jpowcode@gmail.com>
2015-12-30 23:37:27 +0000

    Added powers function unit tests

--- seqpy_test.py
+++ seqpy_test.py
@@ -3,6 +3,7 @@ from seqpy import *
 ############# primes test ################
 
 
+@with_setup(my_setup_function, my_teardown_function)
 def test_primes_0():
     assert primes[0] == 2
 
@@ -23,6 +24,8 @@ def test_primes_seq():
 def test_primes_len():
     primes[9]
     assert len(primes) == 10
+
+
 
 ############# squares test ################
 
@@ -47,3 +50,41 @@ def test_squares_seq():
 def test_squares_len():
     squares[25]
     assert len(squares) == 26
+
+
+############ powers test ################
+
+
+def test_powers_0p0():
+    assert powers(0)[0] == 0
+
+
+def test_powers_1p1():
+    assert powers(1)[1] == 1
+
+
+def test_primes_5p0():
+    assert powers(5)[0] == 0
+
+
+def test_powers_3_seq():
+    powers(3)[4]
+    assert powers.seq() == [0, 3, 9, 27, 83]
+
+
+def test_powers_len():
+    powers(7)[32]
+    assert len(powers) == 33
+
+
+
+"""
+A generator to make multiple test cases
+def test_primes():
+    for i in range(1, 5):
+        yield check_odd, primes[i]
+
+def check_odd(n):
+    assert n % 2 == 1
+
+"""
