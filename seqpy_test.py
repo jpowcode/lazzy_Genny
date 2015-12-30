acc2d3c7c82e35a2eb53d2451ac3ae875c757091
jpowcode <jpowcode@gmail.com>
2015-12-30 22:55:07 +0000

    Added unit tests for squars function

--- seqpy_test.py
+++ seqpy_test.py
@@ -1,5 +1,7 @@
 from seqpy import *
 
+############# primes test ################
+
 
 def test_primes_0():
     assert primes[0] == 2
@@ -14,8 +16,34 @@ def test_primes_5():
 
 
 def test_primes_seq():
-    assert primes.seq() == [2, 3, 5, 7, 11, 13]
+    primes[6]
+    assert primes.seq() == [2, 3, 5, 7, 11, 13, 17]
 
 
 def test_primes_len():
-    assert len(primes) == 6
+    primes[9]
+    assert len(primes) == 10
+
+############# squares test ################
+
+
+def test_squares_0():
+    assert squares[0] == 0
+
+
+def test_squares_1():
+    assert squares[1] == 1
+
+
+def test_primes_7():
+    assert squares[5] == 25
+
+
+def test_squares_seq():
+    squares[10]
+    assert squares.seq() == [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+def test_squares_len():
+    squares[25]
+    assert len(squares) == 26
