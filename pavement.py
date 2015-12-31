b877d22dd6f966535d295089dadc5fca28e96949
jon <jpowcode@gmail.com>
2015-12-31 15:54:24 +0000

    Bsic paver tasks added to run nosetests and pylint

--- pavement.py
+++ pavement.py
@@ -1,1 +1,19 @@
+from paver.tasks import task
+from paver.easy import sh
 
+@task
+def unit_tests():
+	sh('nosetests --with-coverage -s seqpy_tests.py')
+	
+@task
+def run_pylint():
+	try:
+		sh('pylint --msg-template="{path}:{line}:
+		[{msg_id}({symbol}), {obj}] {msg}" bank/ > pylint.txt')
+	except BuildFailure:
+		pass
+		
+@needs('unit_tests',  'run_pylint')
+@task
+def default():
+	pass
